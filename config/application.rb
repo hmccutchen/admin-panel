248f493ef129285860c51aa728828748d090d93b
Hassan Mccutchen <mccutchen.hassan@gmail.com>
2018-09-12 23:43:12 -0400

    dcd

--- config/application.rb
+++ config/application.rb
@@ -5,14 +5,14 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.assets.initialize_on_precompile = false
+
 
 module AdminPanel
   class Application < Rails::Application
     Raven.configure do |config|
   config.dsn = 'https://914393aec7d6439e956ec9064bfee626:c6eb38080dbc4298bf5ab1ce57826fb2@sentry.io/1279152'
 end
-
+config.assets.initialize_on_precompile = false
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
